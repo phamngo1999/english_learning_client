4d7e139c6ea8ca4542dd2c511e0e9ddef52d5153
PhamNgo <phamngo199@gmail.com>
2025-06-08 21:44:12 +0700

    add server_domain

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // Global configuration
 const CONFIG = {
-    SERVER_DOMAIN: 'http://localhost:8080',
-    // Add other global configurations here if needed
+	//SERVER_DOMAIN: 'http://localhost:8080',
+	SERVER_DOMAIN: 'https://english-learning-app-eh89.onrender.com'
+	// Add other global configurations here if needed
 };
